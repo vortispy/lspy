3d3f550a3a8ec04f98cb482a7281e12db7c8e3c4
vortispy <onepiece.no23@gmail.com>
2014-10-06 15:38:19 +0900

    get dir path from command line

--- ls.py
+++ ls.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path", default=".", nargs="?")
+args = parser.parse_args()
 
 def ls():
-    for file in os.listdir("."):
+    path = args.path
+    for file in os.listdir(path):
         if not file[0].startswith("."):
             print file,
 
