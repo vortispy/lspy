32380cd5493f1d49807619f893e7527f08dc1603
vortispy <onepiece.no23@gmail.com>
2014-10-23 15:13:21 +0900

    link recognize

--- ls.py
+++ ls.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import os
+import stat
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -11,56 +12,47 @@ args = parser.parse_args()
 
 def ls():
     path = args.path
-    listdir = os.listdir(path)
+    if args.all:
+        listdir = os.listdir(path)
+    else:
+        listdir = [f for f in os.listdir(path) if not f.startswith(".")]
     listdir.sort()
+    output = ""
     for filename in listdir:
-        if args.all:
-            print filename,
-        elif not filename.startswith("."):
-            print filename,
-        elif args.list:
+        if args.list:
             filepath = os.path.join(path, filename)
-            mode = os.stat(filepath).st_mode
+            mode = os.lstat(filepath).st_mode
             access = convertAccess(mode)
-            print access + " " + filename
+            output += access + " " + filename + "\n"
+        else:
+            output += filename + " "
+    print output
 
 
 def convertAccess(mode):
     access = ["-"] * 10
 
-    S_IFDIR = 004000
-
-    S_IRUSR = 00400
-    S_IWUSR = 00200
-    S_IXUSR = 00100
-    
-    S_IRGRP = 00040
-    S_IWGRP = 00020
-    S_IXGRP = 00010
-
-    S_IROTH = 00004
-    S_IWOTH = 00002
-    S_IXOTH = 00001
-
-    if S_IFDIR & mode:
+    if stat.S_ISDIR(mode):
         access[0] = "d"
-    if S_IRUSR & mode:
+    elif stat.S_ISLNK(mode):
+        access[0] = "l"
+    if stat.S_IRUSR & mode:
         access[1] = "r"
-    if S_IWUSR & mode:
+    if stat.S_IWUSR & mode:
         access[2] = "w"
-    if S_IXUSR & mode:
+    if stat.S_IXUSR & mode:
         access[3] = "x"
-    if S_IRGRP & mode:
+    if stat.S_IRGRP & mode:
         access[4] = "r"
-    if S_IWGRP & mode:
+    if stat.S_IWGRP & mode:
         access[5] = "w"
-    if S_IXGRP & mode:
+    if stat.S_IXGRP & mode:
         access[6] = "x"
-    if S_IROTH & mode:
+    if stat.S_IROTH & mode:
         access[7] = "r"
-    if S_IWOTH & mode:
+    if stat.S_IWOTH & mode:
         access[8] = "w"
-    if S_IXOTH & mode:
+    if stat.S_IXOTH & mode:
         access[9] = "x"
 
     return "".join(access)
